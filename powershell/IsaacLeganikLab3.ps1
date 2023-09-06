05954a1aeb32a30a17e03b78a71c04244ba3b98a
Leganiir <142620485+Leganiir@users.noreply.github.com>
2023-09-05 22:28:20 -0500

    Add files via upload

---
+++ powershell/IsaacLeganikLab3.ps1
@@ -0,0 +1,9 @@
+﻿Send-MailMessage -To "isaacleganik@gmail.com" -From "ileganik9181@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+$BODY = "This machine's IP is $IPv4. User is $User. Hostname is $HostName. PowerShell Version $Version. Today's Date is $Date."
+$IPv4 = (Get-NetIPAddress).ipv4address | Select-String "192*"
+$User = (Get-ChildItem Env:USERNAME).Value
+$HostName = $env:COMPUTERNAME
+$Version = $PSVersionTable.PSVersion.ToString()
+$Date = (Get-Date).ToString("dddd, MMMM dd, yyyy") 
+
+Write-Host($BODY)
